2088e63623946516cb5e873f554b75ebbe59d1d4
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-10-20 15:48:56 -0400

    7. Administracion de Productos

---
+++ app/admin/products/new/page.tsx
@@ -0,0 +1,12 @@
+
+import Heading from "@/components/ui/Heading";
+
+export default function CreateProductPage() {
+  return (
+    <>
+      <Heading>Nuevo Producto</Heading>
+
+    
+    </>
+  )
+}
--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/products/ProductCard";
+import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 async function getProducts(category: string) {
@@ -21,9 +22,8 @@ export default async function OrderPage({
 
   return (
     <>
-      <h1 className="text-2xl my-10">
-        Elige y personaliza tu pedido a continuación
-      </h1>
+      <Heading>Elige y personaliza tu pedido a continuación</Heading>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 items-stretch">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
